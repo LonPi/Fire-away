7a7d9316be4472ae05b1d412c8d031177a2c22e7
Fiona Chye <fiona_chye@hotmail.ca>
2017-05-19 20:35:50 -0400

    implemented character teleport behaviour
    Player can now blink horizontally but not past an obstacle.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,6 +9,7 @@ public class Player : MonoBehaviour {
     float gravity = -20f;
     float moveVelocity = 5f;
     float jumpVelocity = 10f;
+    float blinkDistance = 3f;
     PlayerController controller;
     Vector2 velocity;
     
@@ -19,21 +20,80 @@ public class Player : MonoBehaviour {
 
     private void Update()
     {
-        velocity.x = Input.GetAxisRaw("Horizontal") * moveVelocity;
+        //HandleMovement();
+        HandleSpells();
+    }
 
+    void HandleMovement()
+    {
+        Flip();
+        var _collisionInfo = controller.collisionInfo;
+        
         //prevent gravity buildup and prevent from sticking top
-        if (controller.collisionInfo.below || controller.collisionInfo.above)
+        if (controller.collisionInfo.below || _collisionInfo.above)
         {
             velocity.y = 0f;
         }
 
-        if (Input.GetKeyDown(KeyCode.Space) && controller.collisionInfo.below)
+        if (Input.GetKeyDown(KeyCode.Space) && _collisionInfo.below)
         {
             velocity.y = jumpVelocity;
         }
 
+        velocity.x = Input.GetAxisRaw("Horizontal") * moveVelocity;
         velocity.y += gravity * Time.deltaTime;
         controller.Move(velocity * Time.deltaTime);
+    }
+
+    void HandleSpells()
+    {
+        // TODO: play animation for each spells
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            Debug.Log("melee");
+            // check enemy in melee range
+        }
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            Debug.Log("blink");
+            bool pressLeft = Input.GetKey(KeyCode.LeftArrow);
+            bool pressRight = Input.GetKey(KeyCode.RightArrow);
+
+            // check if the right combo is pressed
+            bool canBlink = pressLeft & !pressRight | !pressLeft & pressRight;
+            
+            if (canBlink)
+            {
+                Debug.Log("Can blink to " + (pressLeft ? "left" : "right"));
+                Vector2 direction = pressLeft ? Vector2.left : Vector2.right;
+                controller.Blink(blinkDistance * direction, direction);
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Debug.Log("fireball");
+            // spawn fireball sprite and check collision for that sprite
+        }
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            Debug.Log("meteor");
+            // same as fireball
+        }
+    }
+
+    void Flip()
+    {
+        var _state = controller.state;
+        Vector2 _localScale = transform.localScale;
+        // sprite is facing -x direction by default
+        if (_state.isMovingRight && _localScale.x > 0 || !_state.isMovingRight && _localScale.x < 0)
+        {
+            _localScale.x *= -1;
+            transform.localScale = _localScale;
+        }
     }
 
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -5,21 +5,23 @@ using UnityEngine;
 [RequireComponent(typeof(BoxCollider2D))]
 public class PlayerController : MonoBehaviour
 {
-
     int verticalRayCount = 4;
-    int horizontalRayCount = 5;
+    int horizontalRayCount = 6;
     float skinWidth = 0.015f;
     float verticalRaySpacing, horizontalRaySpacing;
     BoxCollider2D myCollider;
-    RaycastOrigins raycastOrigins;
+    Vector2 raycastTopLeft, raycastBottomLeft, raycastBottomRight;
     int layerMask;
     public CollisionInfo collisionInfo;
+    public State state;
 
     void Start()
     {
         myCollider = GetComponent<BoxCollider2D>();
         layerMask = 1 << LayerMask.NameToLayer("Collision");
         CalculateRaySpacing();
+        collisionInfo.Reset();
+        state.Reset();
     }
 
     void Update()
@@ -36,36 +38,34 @@ public class PlayerController : MonoBehaviour
 
     void CalculateRaycastOrigins()
     {
-        Bounds bounds = myCollider.bounds;
-        raycastOrigins.bottomLeft = new Vector2(bounds.min.x + skinWidth, bounds.min.y + skinWidth);
-        raycastOrigins.bottomRight = new Vector2(bounds.max.x - skinWidth, bounds.min.y + skinWidth);
-        raycastOrigins.topLeft = new Vector2(bounds.min.x + skinWidth, bounds.max.y - skinWidth);
-        raycastOrigins.topRight = new Vector2(bounds.max.x - skinWidth, bounds.max.y - skinWidth);
+        Vector2 _localScale = transform.localScale;
+        
+        Vector2 size = new Vector2(myCollider.size.x * Mathf.Abs(_localScale.x), myCollider.size.y * Mathf.Abs(_localScale.y)) / 2;
+        Vector2 center = new Vector2(myCollider.offset.x * _localScale.x, myCollider.offset.y * _localScale.y);
+
+        raycastTopLeft = transform.position + new Vector3(center.x - size.x + skinWidth, center.y + size.y - skinWidth);
+        raycastBottomRight = transform.position + new Vector3(center.x + size.x - skinWidth, center.y - size.y + skinWidth);
+        raycastBottomLeft = transform.position + new Vector3(center.x - size.x + skinWidth, center.y - size.y + skinWidth);
     }
 
     public void Move(Vector2 deltaMovement)
     {
-        
         collisionInfo.Reset();
         if (deltaMovement.y != 0)
             HandleVerticalMovement(ref deltaMovement);
         if (deltaMovement.x != 0)
             HandleHorizontalMovement(ref deltaMovement);
 
-        //if (collisionInfo.above || collisionInfo.below)
-        //{
-        //    deltaMovement.y = 0;
-        //}
-
+        
         transform.Translate(deltaMovement);
     }
 
     void HandleVerticalMovement(ref Vector2 deltaMovement)
     {
-        bool isFalling = deltaMovement.y < 0;
+        state.isFalling = deltaMovement.y < 0;
         float raycastDistance = Mathf.Abs(deltaMovement.y) + skinWidth;
-        Vector2 rayOrigin = isFalling ? raycastOrigins.bottomLeft : raycastOrigins.topLeft;
-        Vector2 direction = isFalling ? Vector2.down : Vector2.up;
+        Vector2 rayOrigin = state.isFalling ? raycastBottomLeft : raycastTopLeft;
+        Vector2 direction = state.isFalling ? Vector2.down : Vector2.up;
         for (int i = 0; i < verticalRayCount; i++)
         {
             Vector2 rayVector = new Vector2(rayOrigin.x + i * verticalRaySpacing, rayOrigin.y);
@@ -75,7 +75,7 @@ public class PlayerController : MonoBehaviour
             {
                 deltaMovement.y = hit.point.y - rayVector.y;
                 raycastDistance = Mathf.Abs(deltaMovement.y) + skinWidth;
-                if (isFalling)
+                if (state.isFalling)
                 {
                     deltaMovement.y += skinWidth;
                     collisionInfo.below = true;
@@ -91,10 +91,10 @@ public class PlayerController : MonoBehaviour
 
     void HandleHorizontalMovement(ref Vector2 deltaMovement)
     {
-        bool isMovingRight = deltaMovement.x > 0;
+        state.isMovingRight = deltaMovement.x > 0;
         float raycastDistance = Mathf.Abs(deltaMovement.x) + skinWidth;
-        Vector2 rayOrigin = isMovingRight ? raycastOrigins.bottomRight : raycastOrigins.bottomLeft;
-        Vector2 direction = isMovingRight ? Vector2.right : Vector2.left;
+        Vector2 rayOrigin = state.isMovingRight ? raycastBottomRight : raycastBottomLeft;
+        Vector2 direction = state.isMovingRight ? Vector2.right : Vector2.left;
 
         for (int i = 0; i < horizontalRayCount; i++)
         {
@@ -106,15 +106,57 @@ public class PlayerController : MonoBehaviour
             {
                 deltaMovement.x = hit.point.x - rayVector.x;
                 raycastDistance = Mathf.Abs(deltaMovement.x) + skinWidth;
-                if (isMovingRight) deltaMovement.x -= skinWidth;
-                else deltaMovement.x += skinWidth;
+                if (state.isMovingRight)
+                {
+                    deltaMovement.x -= skinWidth;
+                    collisionInfo.right = true;
+                }
+                else
+                {
+                    deltaMovement.x += skinWidth;
+                    collisionInfo.left = true;
+                }
             }
         }
     }
 
-    struct RaycastOrigins
+    public void Blink(Vector2 deltaMovement, Vector2 direction)
     {
-        public Vector2 topLeft, topRight, bottomLeft, bottomRight;
+        // raycast to blink destination
+        float raycastDistance = Mathf.Abs(deltaMovement.x) + skinWidth;
+        Vector2 raycastOrigin = state.isMovingRight ? raycastBottomRight : raycastBottomLeft;
+        
+        for (int i = 0; i < horizontalRayCount; i++)
+        {
+            Vector2 rayVector = new Vector2(raycastOrigin.x, raycastOrigin.y + i * horizontalRaySpacing);
+            Debug.DrawRay(rayVector, raycastDistance * direction, Color.green);
+            RaycastHit2D hit = Physics2D.Raycast(rayVector, direction, raycastDistance, layerMask);
+            if (!hit) continue;
+            deltaMovement.x = hit.point.x - rayVector.x;
+            raycastDistance = Mathf.Abs(deltaMovement.x) + skinWidth;
+            if (state.isMovingRight)
+            {
+                deltaMovement.x -= skinWidth;
+                collisionInfo.right = true;
+            }
+            else
+            {
+                deltaMovement.x += skinWidth;
+                collisionInfo.left = true;
+            }
+        }
+        
+        // teleport to destination
+        transform.Translate(deltaMovement);
+    }
+
+    public struct State
+    {
+        public bool isMovingRight, isFalling;
+        public void Reset()
+        {
+            isMovingRight = isFalling = false;
+        }
     }
 
     public struct CollisionInfo
