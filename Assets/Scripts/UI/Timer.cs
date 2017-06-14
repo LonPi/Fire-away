cbdc963a279c7b2db4aad30ec7b5300c738f9b9d
Fiona Chye <fiona_chye@hotmail.ca>
2017-06-14 15:06:39 -0400

    patch update

--- Assets/Scripts/UI/Timer.cs
+++ Assets/Scripts/UI/Timer.cs
@@ -14,8 +14,8 @@ public class Timer : MonoBehaviour {
 	
 	void Update () {
         time += Time.deltaTime;
-        var minutes = time / 60;
-        var seconds = time % 60;
+        var minutes = Mathf.FloorToInt(time / 60);
+        var seconds = Mathf.FloorToInt(time % 60);
 
         //update the label value
         timerText.text = string.Format("{0:00}:{1:00}", minutes, seconds);
